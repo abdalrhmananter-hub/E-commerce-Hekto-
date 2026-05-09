a3e28ba8b12cb05b589170efb31e90a59663f1c9
abdalrhmananter-hub <abdalrhmananter@gmail.com>
2026-05-09 14:01:13 +0300

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const response = await axios.get('http://localhost:5000/products');
+                const response = await axios.get('http://e-commerce-hekto-back-end.vercel.app/products');
                 setProducts(response.data.allProducts);
             } catch (error) {
                 console.error("Error fetching products:", error);
