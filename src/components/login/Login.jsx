3e5b37eab4c0d909d032e60e7927d70ee4766b87
abdalrhmananter-hub <abdalrhmananter@gmail.com>
2026-05-09 14:03:26 +0300

    Update Login.jsx

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -28,7 +28,7 @@ export default function Login() {
             setServerError("");
 
 
-            const response = await axios.post('http://e-commerce-hekto-back-end.vercel.app/users/login', data);
+            const response = await axios.post('https://e-commerce-hekto-back-end.vercel.app/users/login', data);
 
 
             localStorage.setItem('userToken', response.data.userData.token);
